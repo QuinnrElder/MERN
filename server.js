a1ae195454425702fb62dc6b72c95b0284843ffc
Quinn Elder <qre.turing@gmail.com>
2020-10-22 13:54:27 -0600

    Add unique paths for application

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const app = express();
 // CONNECT DB
 connectDB();
 
+// INIT MIDDLEWARE
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("API RUNNING");
 });
+
+// DEFINE ROUTES
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
